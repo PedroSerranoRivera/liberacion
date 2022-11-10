194db5a06c6f4b12a319e87d948e6df447094bf2
PedroSerranoRivera <69986946+PedroSerranoRivera@users.noreply.github.com>
2022-11-10 17:28:34 -0600

    Update excelor.py

--- excelor.py
+++ excelor.py
@@ -1,99 +1,46 @@
-from ctypes.wintypes import tagRECT
-from distutils import extension
-from fileinput import filename
-from hashlib import new
-from msilib.schema import Patch
-from sys import float_repr_style
-from turtle import pd
-from openpyxl import Workbook
-#from openpyxl.utils import get_column_letterfrom
-from openpyxl import load_workbook
+#Liberias
 import pandas as pd
-import glob, os, shutil, sys
-from PIL import Image
-from importlib.metadata import files
-from logging import lastResort
-from pathlib import Path
+import os
+import shutil
 from datetime import datetime
+import xlwings as xw
 
-# Cargar el archivo xls más reciente
+#Constantes
+nombres = {'SDESC':'MERCADO', 'SDESC.1':'PERIODO', 'SDESC.2':'FABRICANTE', 'VTAS. EN VALOR':'VENTAS EN VALOR (in 000 PESOS)',
+           'VTAS. EN UNIDADES EQ (in LTS)':'VENTAS EN UNIDADES EQ (in 000 KILOS)', 'DIST. NUM. MANEJANTES':'DISTRIBUCION NUMERICA MANEJANTES',
+           'VTAS. EN UNIDADES EQ (in LTS).1':'VENTAS EN UNIDADES (in 000)','INVENTARIO TOTAL EN UNID. EQ. (in LTS)':'INVENTARIO TOTAL EN UNIDADES (in 000)',
+           'COMPRAS EN UNIDADES EQ. (in LTS)':'COMPRAS EN UNIDADES (in 000)','SHARE VENTAS UNIDADES EQ. (in LTS)':'SHARE VENTAS EN VALOR',
+           'VTAS. EN UNIDADES':'VENTAS EN UNIDADES EQUIVALENTES 3 (in 00'}
+base_csv = str()
 
-folder = Path(os.getcwd())
+fecha = datetime.today().strftime('%Y_%m_%d_%H_%M')
 
-def basuraitor():
-    for archivo_ruta in os.scandir(folder):
-        if archivo_ruta.is_file() and archivo_ruta.name.endswith('.csv'):
-           # nombre archivo
-           print('este es el archivo ruta', archivo_ruta)
-           origen_archivo = archivo_ruta
-           # ruta completa
-           print('esta es archivo ruta path', archivo_ruta.path)
-           # nombre del archivo 
-           print('este es el archivo_ruta.name', archivo_ruta.name)
-           new_dir = archivo_ruta.name.rsplit('.', 1)[0]
-           print('Este es el archivo', new_dir)
-           #print(new_dir)
-        try:
-           # Crea el directorio uniendo ruta y nombre del archivo
-           fechar = (datetime.today().strftime('_''%Y_%m_%d_%H_%M'))
-           print(fechar)
-           ruta_nueva_NoDate = str(Path(os.path.join(folder, new_dir)))
-           print(ruta_nueva_NoDate)
-           ruta_nueva = ruta_nueva_NoDate + fechar
-           print(ruta_nueva)
-           os.makedirs(ruta_nueva)
-        except WindowsError:
-        # Ventana de error si el directorio existe
-        # Handle the case where the target dir already exist.
-            pass
-    #Mueve el archivo al directorio creado
-            directorio_nuevo = Path(ruta_nueva)
-            print('esta es directorio nuevo',directorio_nuevo)
-            shutil.move(Path(origen_archivo), Path(directorio_nuevo))
+for i in os.listdir():
+    if '.csv' in i:
+        base_csv = i
 
-basuraitor()
+#Cargamos la base
+base = pd.read_csv(base_csv)
+#Renombramos las columnas
+base.rename(columns = nombres, inplace = True)
+
+#Insertamos el dataframe decil_fugados en la hoja Decil Fugados
+app = xw.App(visible=False)
+book = xw.Book('LIBERACION_ORIGINAL.xlsx')
+INFORMACION = book.sheets['INFORMACION']
 
 
+INFORMACION.range('A1').options(index=False).value = base
+nombre_liberacion = 'LIBERACION_ORIGINAL_' + fecha + '_MOD.xlsx'
+book.save(nombre_liberacion)
+book.close()
+app.quit()
 
+#Creamos carpeta
+os.mkdir(base_csv[:-4] + '_' + fecha )
+nombre_carpeta = base_csv[:-4] + '_' + fecha
 
+os.getcwd() + '\\' + base_csv[:-4] + '_' + fecha
 
-
-
-
-
-
-
-
-
-# Cargar el archivo y imprime hojas
-'''
-
-wb = load_workbook(filename = "Muebles_dico_pa_Abril_22.xlsx")
-print(wb.sheetnames)
-# elegir hoja
-wb.active = wb['Soporte Técnico Avanzado']
-#
-hoja = wb.active
-#print(hoja)
-celda = hoja['C37']
-celda1 = hoja['C38']
-
-# 
-
-"""
-if celda.value == celda1.value:
-  print("Value of the Cell 1:",celda.value)
-  print("ahuevirri")
-else: 
-  print("seas mamon") 
-"""
-
-valor = 2700
-
-if celda.value == valor:
-  print("Value of the Cell 1:",celda.value)
-  print("ahuevirri")
-else: 
-  print("seas mamon") 
-
-'''
+shutil.move(os.getcwd() + '\\' + nombre_liberacion, os.getcwd() + '\\' + nombre_carpeta)
+shutil.move(os.getcwd() + '\\' + base_csv, os.getcwd() + '\\' + nombre_carpeta)
